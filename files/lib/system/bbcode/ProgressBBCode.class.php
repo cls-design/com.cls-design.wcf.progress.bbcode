2588c31fffcaa87dd774e53a4a81a43336b88b00
Tom <tom@cls-design.com>
2023-10-03 23:07:55 +0200

    Update ProgressBBCode.class.php

--- files/lib/system/bbcode/ProgressBBCode.class.php
+++ files/lib/system/bbcode/ProgressBBCode.class.php
@@ -1,41 +1,43 @@
 <?php
+
 namespace wcf\system\bbcode;
-use wcf\system\WCF;
-use wcf\system\exception\SystemException;
+
+use wcf\util\MessageUtil;
 use wcf\util\StringUtil;
 
 /**
- * ProgressBarBBCode.
+ * ProgressBBCode.
  * 
- * @package	com.cls-design.wcf.progress.bbcode
+ * @package		com.cls-design.wcf.progress.bbcode
  * @copyright	www.cls-design.com
- * @author	www.cls-design.com
+ * @author		www.cls-design.com
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
 final class ProgressBBCode extends AbstractBBCode {
 	/**
-	* @see	wcf\system\bbcode\IBBCode::getParsedTag()
-	*/
-	public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser): string {
-
-		$progress = array(
-			'progressbarWidth' => (!empty($openingTag['attributes'][0])) ? StringUtil::trim($openingTag['attributes'][0]) : "",
-			'legendColorscheme' => (!empty($openingTag['attributes'][1])) ? StringUtil::trim($openingTag['attributes'][1]) : "0",
-		);
-
-		$content = (!empty($content)) ? StringUtil::trim($content) : "";
-		if($parser->getOutputType() == 'text/html') {
-			$progressCode ='<div class="progress-bar-wrapper">';
-			$progressCode .= '<div>' . $content . '</div>';
-			$progressCode .= '<div class="progress-bar"><div class="progress-bar-color-scheme-'. $progress["legendColorscheme"] . '"><div style="width: '  $progress["progressbarWidth"] . '%;"> </div></div>';
-			$progressCode .= '<div>' . $progress["progressbarWidth"] . '%</div></div>';
-			$progressCode .= '</div>';
-
-			return $progressCode;
-		} else if($parser->getOutputType() == 'text/simplified-html') {
-
-			return '';
+	 * @inheriDoc
+	 */
+	public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser) : string {
+		$progressbarWidth = (int) ($openingTag['attributes'][0] ?? 0);
+		$progressColorscheme = StringUtil::trim($openingTag['attributes'][1] ?? 'default');
+		$content = MessageUtil::stripCrap(StringUtil::trim($content));
+		
+		if ($parser->getOutputType() == 'text/html') {
+			$contentWrapper = '' === $content ? '' : "<div>{$content}</div>";
+			
+			return <<<HTML
+			<div class="progress-bar-wrapper">
+				{$contentWrapper}
+				<div class="progress-bar">
+					<div class="progress-bar-color-scheme-{$progressColorscheme}">
+						<div style="width: {$progressbarWidth}%;"></div>
+					</div>
+					<div>{$progressbarWidth}%</div>
+				</div>
+			</div>
+			HTML;
 		}
+		
 		return '';
 	}
 }
