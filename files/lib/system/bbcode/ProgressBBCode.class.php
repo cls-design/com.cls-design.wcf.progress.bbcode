07d721fe0154980dc0a30c93c4aaf26d01347f75
Tom <tom@cls-design.com>
2023-10-17 10:19:54 +0200

    Backport WoltLab Suite 5.5

--- files/lib/system/bbcode/ProgressBBCode.class.php
+++ files/lib/system/bbcode/ProgressBBCode.class.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace wcf\system\bbcode;
-
+use wcf\system\WCF;
 use wcf\util\MessageUtil;
 use wcf\util\StringUtil;
 
@@ -18,25 +18,27 @@ final class ProgressBBCode extends AbstractBBCode {
 	 * @inheriDoc
 	 */
 	public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser) : string {
-		$progressbarWidth = (int) ($openingTag['attributes'][0] ?? '25%');
+		$progressbarWidth = (int) ($openingTag['attributes'][0] ?? '0');
 		$progressColorscheme = (int) ($openingTag['attributes'][1] ?? '0');
 		$content = MessageUtil::stripCrap(StringUtil::trim($content));
-		
+		$progressTitle = WCF::getLanguage()->get('wcf.bbcode.progress');
+
 		if ($parser->getOutputType() == 'text/html') {
-			
 			return <<<HTML
-			<div class="progress-bar-wrapper">
+			<div class="progress-bar-wrapper progress-bar-color-scheme-{$progressColorscheme}">
 				{$content}
 				<div class="progress-bar">
-					<div class="progress-bar-color-scheme-{$progressColorscheme}">
-						<div style="width: {$progressbarWidth}%;"></div>
-					</div>
+					<progress min="0" max="100"  value="{$progressbarWidth}"></progress>
 					<div>{$progressbarWidth}%</div>
 				</div>
 			</div>
 			HTML;
+		} elseif ($parser->getOutputType() == 'text/simplified-html') {
+			return '<div class="progress-bar-wrapper">'. $progressTitle . ' ' . $content . ': ' . $progressbarWidth . '%</div>';
+		} else {
+		 	return $progressTitle . ' ' . $content . ': ' . $progressbarWidth;
 		}
-		
+
 		return '';
 	}
 }
